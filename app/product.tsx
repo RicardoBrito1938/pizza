33608f9b6abebae459bfe2bd792ac600cfbb5cd6
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2025-03-11 10:31:53 +0000

    Add form for pizza registration with input fields for name, description, and sizes/prices

--- app/product.tsx
+++ app/product.tsx
@@ -7,6 +7,7 @@ import { LinearGradient } from "expo-linear-gradient";
 import {
   KeyboardAvoidingView,
   Platform,
+  ScrollView,
   Text,
   TouchableOpacity,
   View,
@@ -14,6 +15,8 @@ import {
 import { getStatusBarHeight } from "react-native-iphone-x-helper";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
+import { InputPrice } from "@/components/ui/input-price";
+import { Input } from "@/components/ui/input";
 
 const Container = styled(KeyboardAvoidingView, {
   flex: 1,
@@ -55,6 +58,35 @@ const PickImageButton = styled(Button, {
   marginLeft: 32,
 });
 
+const Form = styled(View, {
+  padding: 24,
+  width: "100%",
+});
+
+const Label = styled(Text, {
+  color: extendedTheme.colors.$secondary900,
+  fontFamily: extendedTheme.fonts.$textFont,
+});
+
+const InputGroup = styled(View, {
+  width: "100%",
+  marginBottom: 16,
+});
+
+const InputGroupHeader = styled(View, {
+  width: "100%",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
+
+const MaxCharacters = styled(Text, {
+  fontSize: 10,
+  marginBottom: 8,
+  color: extendedTheme.colors.$secondary900,
+  fontFamily: extendedTheme.fonts.$textFont,
+});
+
 export default function Product() {
   const [image, setImage] = useState<string | null>(null);
   const handleImagePick = async () => {
@@ -78,27 +110,54 @@ export default function Product() {
   };
   return (
     <Container behavior={Platform.OS === "ios" ? "padding" : "height"}>
-      <Header
-        colors={[
-          extendedTheme.tokens.$gradientStart,
-          extendedTheme.tokens.$gradientEnd,
-        ]}
-      >
-        <ButtonBack />
-        <Title>Register</Title>
-        <TouchableOpacity>
-          <DeleteLabel>Delete</DeleteLabel>
-        </TouchableOpacity>
-      </Header>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Header
+          colors={[
+            extendedTheme.tokens.$gradientStart,
+            extendedTheme.tokens.$gradientEnd,
+          ]}
+        >
+          <ButtonBack />
+          <Title>Register</Title>
+          <TouchableOpacity>
+            <DeleteLabel>Delete</DeleteLabel>
+          </TouchableOpacity>
+        </Header>
 
-      <Upload>
-        <Photo uri={image} />
-        <PickImageButton
-          title="Pick"
-          variant="primary"
-          onPress={handleImagePick}
-        />
-      </Upload>
+        <Upload>
+          <Photo uri={image} />
+          <PickImageButton
+            title="Pick"
+            variant="primary"
+            onPress={handleImagePick}
+          />
+        </Upload>
+
+        <Form>
+          <InputGroup>
+            <Label>Name</Label>
+            <Input />
+          </InputGroup>
+
+          <InputGroup>
+            <InputGroupHeader>
+              <Label>Description</Label>
+              <MaxCharacters>0 to 60 characters</MaxCharacters>
+            </InputGroupHeader>
+
+            <Input multiline maxLength={60} style={{ height: 80 }} />
+          </InputGroup>
+
+          <InputGroup>
+            <Label>Sizes and prices</Label>
+            <InputPrice size="S" />
+            <InputPrice size="M" />
+            <InputPrice size="L" />
+          </InputGroup>
+
+          <Button title="Register pizza" variant="secondary" />
+        </Form>
+      </ScrollView>
     </Container>
   );
 }
---
+++ components/ui/input-price.tsx
@@ -0,0 +1,52 @@
+import extendedTheme from "@/styles/extendedTheme";
+import { styled } from "@fast-styles/react";
+import type { WithStyles } from "@fast-styles/react/lib/typescript/types";
+import { Text, TextInput, type TextInputProps, View } from "react-native";
+
+const Container = styled(View, {
+  width: "100%",
+  height: 56,
+  borderWidth: 1,
+  borderRadius: 12,
+  borderColor: extendedTheme.colors.$shape,
+  marginBottom: 8,
+  flexDirection: "row",
+  alignItems: "center",
+});
+
+const Size = styled(View, {
+  width: 56,
+  height: 56,
+  justifyContent: "center",
+  alignItems: "center",
+  borderRightWidth: 1,
+  borderRightColor: extendedTheme.colors.$shape,
+  marginRight: 8,
+});
+
+const Label = styled(Text, {
+  fontSize: 14,
+  fontFamily: extendedTheme.fonts.$textFont,
+  color: extendedTheme.colors.$secondary900,
+});
+
+const Input = styled(TextInput, {
+  flex: 1,
+  marginLeft: 8,
+});
+
+type Props = WithStyles<TextInputProps> & {
+  size: string;
+};
+
+export const InputPrice = ({ size, ...rest }: Props) => {
+  return (
+    <Container>
+      <Size>
+        <Label>{size}</Label>
+      </Size>
+      <Label>£</Label>
+      <Input keyboardType="numeric" {...rest} />
+    </Container>
+  );
+};
