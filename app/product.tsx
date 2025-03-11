fedc5518decccdf0ebb7e4cdc387f0c85c710d82
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2025-03-11 09:06:03 +0000

    Add image picker functionality to Product component and create Photo UI component

--- app/product.tsx
+++ app/product.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import { ButtonBack } from "@/components/ui/button-back";
+import { Photo } from "@/components/ui/photo";
 import extendedTheme from "@/styles/extendedTheme";
 import { styled } from "@fast-styles/react";
 import { LinearGradient } from "expo-linear-gradient";
@@ -7,7 +9,11 @@ import {
   Platform,
   Text,
   TouchableOpacity,
+  View,
 } from "react-native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 
 const Container = styled(KeyboardAvoidingView, {
   flex: 1,
@@ -20,7 +26,7 @@ const Header = styled(LinearGradient, {
   justifyContent: "space-between",
   width: "100%",
   padding: 24,
-  paddingTop: 48,
+  paddingTop: getStatusBarHeight() + 48,
 });
 
 const Title = styled(Text, {
@@ -36,7 +42,40 @@ const DeleteLabel = styled(Text, {
   fontFamily: extendedTheme.fonts.$textFont,
 });
 
+const Upload = styled(View, {
+  width: "100%",
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "center",
+  marginVertical: 32,
+});
+
+const PickImageButton = styled(Button, {
+  maxWidth: 90,
+  marginLeft: 32,
+});
+
 export default function Product() {
+  const [image, setImage] = useState<string | null>(null);
+  const handleImagePick = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (status !== "granted") {
+      alert("Sorry, we need camera roll permissions to make this work!");
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: "images",
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
   return (
     <Container behavior={Platform.OS === "ios" ? "padding" : "height"}>
       <Header
@@ -51,6 +90,15 @@ export default function Product() {
           <DeleteLabel>Delete</DeleteLabel>
         </TouchableOpacity>
       </Header>
+
+      <Upload>
+        <Photo uri={image} />
+        <PickImageButton
+          title="Pick"
+          variant="primary"
+          onPress={handleImagePick}
+        />
+      </Upload>
     </Container>
   );
 }
---
+++ components/ui/photo.tsx
@@ -0,0 +1,44 @@
+import extendedTheme from "@/styles/extendedTheme";
+import { styled } from "@fast-styles/react";
+import { type ImageProps, Image as RNImage, Text, View } from "react-native";
+
+export const Image = styled(RNImage, {
+  width: 160,
+  height: 160,
+  borderRadius: 80,
+});
+
+export const Placeholder = styled(View, {
+  width: 160,
+  height: 160,
+  borderRadius: 80,
+  justifyContent: "center",
+  alignItems: "center",
+
+  borderWidth: 1,
+  borderStyle: "dashed",
+  borderColor: extendedTheme.colors.$secondary900,
+});
+
+export const PlaceholderTitle = styled(Text, {
+  fontSize: 14,
+  textAlign: "center",
+  fontFamily: extendedTheme.fonts.$textFont,
+  color: extendedTheme.colors.$secondary900,
+});
+
+type Props = {
+  uri?: string | null;
+  rest?: ImageProps;
+};
+
+export const Photo = ({ uri, ...rest }: Props) => {
+  return uri ? (
+    <Image source={{ uri }} {...rest} />
+  ) : (
+    <Placeholder>
+      {/* biome-ignore lint/style/noUnusedTemplateLiteral: <explanation> */}
+      <PlaceholderTitle>No photo{`\n`} loaded</PlaceholderTitle>
+    </Placeholder>
+  );
+};
