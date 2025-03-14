9ad0a058c7b7e6a307e95aa9fadf3a91fe380e93
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2025-03-14 11:09:47 +0000

    Refactor ButtonBack component to use router for navigation and improve type definitions

--- components/ui/button-back.tsx
+++ components/ui/button-back.tsx
@@ -1,26 +1,30 @@
-import extendedTheme from "@/styles/extendedTheme";
-import { MaterialIcons } from "@expo/vector-icons";
-import { styled } from "@fast-styles/react";
-import { Pressable, type PressableProps } from "react-native";
+import extendedTheme from '@/styles/extendedTheme'
+import { MaterialIcons } from '@expo/vector-icons'
+import { styled } from '@fast-styles/react'
+import type { WithStyles } from '@fast-styles/react/lib/typescript/types'
+import { Pressable, type PressableProps } from 'react-native'
+import { useRouter } from 'expo-router'
 
 const Container = styled(Pressable, {
-  width: 40,
-  height: 40,
-  justifyContent: "center",
-  alignItems: "center",
-  borderRadius: 12,
-  borderWidth: 1,
-  borderColor: extendedTheme.colors.$primary100,
-});
+	width: 40,
+	height: 40,
+	justifyContent: 'center',
+	alignItems: 'center',
+	borderRadius: 12,
+	borderWidth: 1,
+	borderColor: extendedTheme.colors.$primary100,
+})
 
-export const ButtonBack = ({ ...rest }: PressableProps) => {
-  return (
-    <Container>
-      <MaterialIcons
-        name="chevron-left"
-        size={18}
-        color={extendedTheme.colors.$title}
-      />
-    </Container>
-  );
-};
+export const ButtonBack = ({ ...rest }: WithStyles<PressableProps>) => {
+	const router = useRouter()
+
+	return (
+		<Container {...rest} onPress={() => router.back()}>
+			<MaterialIcons
+				name='chevron-left'
+				size={18}
+				color={extendedTheme.colors.$title}
+			/>
+		</Container>
+	)
+}
