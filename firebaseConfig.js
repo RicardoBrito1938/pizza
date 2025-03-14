cf6cf0448ac6672ba61fadfead3f65dbb5791e36
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2025-03-14 11:04:41 +0000

    Refactor firebase configuration for improved readability and consistency

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { getFirestore } from "firebase/firestore"; // Add this import
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { getFirestore } from 'firebase/firestore' // Add this import
 
 // Optionally import the services that you want to use
 // import {...} from 'firebase/auth';
@@ -24,21 +24,21 @@ import { getFirestore } from "firebase/firestore"; // Add this import
 // };
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
+	apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE,
+	messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+}
 
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig)
 export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
-});
-export const db = getFirestore(app); // Initialize and export Firestore
+	persistence: getReactNativePersistence(AsyncStorage),
+})
+export const db = getFirestore(app) // Initialize and export Firestore
 
-const analytics = getAnalytics(app);
+const analytics = getAnalytics(app)
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
