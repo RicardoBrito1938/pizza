a5973b1e29ba38b83b213c568fd1bd946178b0e2
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2025-03-26 12:54:23 +0000

    refactor: enhance SplashScreen animation handling; prevent navigation until animation completes

--- app/splash.tsx
+++ app/splash.tsx
@@ -1,6 +1,6 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { View, Text as RNText } from 'react-native'
-import Animated, { FadeIn } from 'react-native-reanimated'
+import Animated, { FadeIn, runOnJS } from 'react-native-reanimated'
 import { SplashScreen as ExpoSplashScreen, useRouter } from 'expo-router'
 import useSWR from 'swr'
 import { supabase } from '@/supabase/supabase'
@@ -46,20 +46,31 @@ const fetchUser = async () => {
 export default function SplashScreen() {
 	const router = useRouter()
 	const { data: user, error, isLoading } = useSWR('/user', fetchUser)
+	const [animationComplete, setAnimationComplete] = useState(false)
 
 	useEffect(() => {
-		if (isLoading) return
+		if (!animationComplete || isLoading) return
 
 		if (!user || error) {
 			router.replace('/sign-in')
 		} else {
 			router.replace('/(admin)/home')
 		}
-	}, [user, isLoading, router, error])
+	}, [user, isLoading, router, error, animationComplete])
+
+	const handleAnimationComplete = () => {
+		setAnimationComplete(true)
+	}
 
 	return (
 		<Container>
-			<Text entering={FadeIn.duration(1500)}>Welcome to Pizza App!</Text>
+			<Text
+				entering={FadeIn.duration(1500).withCallback(() =>
+					runOnJS(handleAnimationComplete)(),
+				)}
+			>
+				Welcome to Pizza App!
+			</Text>
 		</Container>
 	)
 }
